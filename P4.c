07df8da46b6428a4e501b6cbdb61b459b5d0bf99
Marcos Sirio <37821666+mrsirio@users.noreply.github.com>
2018-05-05 15:15:24 -0300

    Create P4.c

---
+++ P4.c
@@ -0,0 +1,20 @@
+//Marcos Barros de Oliveira Sírio
+//Matrícula: 11721EBI018
+
+#include <stdio.h>
+
+int main(){
+	char string[100];
+	int i, j=0, string2[100];
+	printf("\nDigite uma sequencia: ");
+	scanf("%s", &string);
+	printf("\nSequencia so com numeros: ");
+	for(i=0; string[i]!='\0'; i++){
+		if('0'<=string[i] && string[i]<='9'){
+			printf("%c", string[i]);
+			string2[j]=string[i];
+			j++;}
+		}
+	printf("\n\n\n");
+  return 0;
+}
