339042003edd1a1f39443c4b0bf856e45e8ac4d9
Marcos Sirio <37821666+mrsirio@users.noreply.github.com>
2018-05-21 22:23:22 -0300

    Rename P6.c to P7.c

---
+++ P7.c
@@ -0,0 +1,25 @@
+//Marcos Barros de Oliveira Sirio 
+//Matricula 11721EBI018
+
+#include <stdio.h>
+long int funcao_ackermann(int m,int n){
+	long int x;
+	if(m==0){return n+1;		
+	}
+	if(m>0 && n==0){return funcao_ackermann(m-1,1);
+	}
+	x=funcao_ackermann(m,n-1);
+	if(m>0 && n>0){return funcao_ackermann(m-1,x);
+	}
+}
+int main(){
+	int m, n;
+	printf("\nDigite um valor para m: ");
+	scanf("%d", &m);
+	getchar();
+	printf("\nDigite um valor para n: ");
+	scanf("%d", &n);
+	getchar();
+	printf("\n Valor resultante: %d", funcao_ackermann(m,n));
+return 0;
+}
