4936596c3a1b7775864d0c0938fb86114705ba57
Marcos Sirio <37821666+mrsirio@users.noreply.github.com>
2018-06-20 20:59:44 -0300

    Update P4.c

--- P4.c
+++ P4.c
@@ -8,6 +8,7 @@ int main(){
 	int i, j=0, string2[100];
 	printf("\nDigite uma sequencia: ");
 	scanf("%s", &string);
+	getchar();
 	printf("\nSequencia so com numeros: ");
 	for(i=0; string[i]!='\0'; i++){
 		if('0'<=string[i] && string[i]<='9'){
